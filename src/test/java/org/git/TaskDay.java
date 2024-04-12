7b9f9a7a02ef0f7208ff42b93bfeb10f36df1ec5
GayathriMani12 <gayathrigayu505@gmail.com>
2024-04-12 11:28:50 +0530

    Login

---
+++ src/test/java/org/git/TaskDay.java
@@ -0,0 +1,18 @@
+package org.git;
+
+public class TaskDay {
+	
+	private void empName() {
+		System.out.println("Gayu");
+
+	}
+	
+	private void empId() {
+		System.out.println("1234");
+
+	}
+	
+	
+	
+
+}
